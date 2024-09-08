1ed1df509028be3b4f44c520fb1945ce583402d4
Anu Alias(UST <284271@ust.com>
2024-09-08 22:25:56 +0530

    Added remove Functionality

--- src/advancejava/assign1/TaskApplication.java
+++ src/advancejava/assign1/TaskApplication.java
@@ -10,7 +10,10 @@ public class TaskApplication {
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(System.in);
-			addTask(scanner);
+
+			Task[] taskList = addTask(scanner);
+			System.out.println("Remove Task ");
+			Task[] updatedList = removeTask(scanner, taskList);
 
 		} catch (Exception e) {
 
@@ -19,24 +22,43 @@ public class TaskApplication {
 		}
 	}
 
+//Add remove task functionality
+	private static Task[] removeTask(Scanner scanner, Task[] taskList) {
+		System.out.println("Enter the task Description to remove");
+		String taskDesc = scanner.next();
+		int size = taskList.length;
+		Task[] updatedtasks = new Task[size - 1];
 
-	private static void addTask(Scanner scanner) {
+		for (int i = 0; i < size - 1; i++) {
+			if (taskList[i].getDescription() != taskDesc) {
+				updatedtasks[i] = taskList[i];
+			}
+		}
+		return updatedtasks;
+
+	}
+
+//Add Tasks to the to-do List
+	private static Task[] addTask(Scanner scanner) {
+
 		System.out.println("Enter number of tasks");
 		int n = scanner.nextInt();
 		Task[] taskList = new Task[n];
-
+		System.out.println("Add task Details");
 		for (int i = 0; i < n; i++) {
 
 			System.out.println("Enter the Description");
 			String taskDescription = scanner.next();
-			System.out.println("Enter the Due Date in yyyy-MM-dd format");
+			System.out.println("Enter the Due Date in dd-MM-yyyy format");
 			String userInput = scanner.next();
-			DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 			LocalDate date = LocalDate.parse(userInput, dateFormat);
 			Task task = new Task(taskDescription, date);
 
 			taskList[i] = task;
 		}
+
+		return taskList;
 	}
 
 }
