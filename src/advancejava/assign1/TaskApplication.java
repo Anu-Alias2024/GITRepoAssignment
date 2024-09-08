94df3c20444ba0d8c15d982744ebdb95a7297def
Anu Alias(UST <284271@ust.com>
2024-09-08 21:36:47 +0530

    Add task Functionality Implemented and refactored

--- src/advancejava/assign1/TaskApplication.java
+++ src/advancejava/assign1/TaskApplication.java
@@ -10,27 +10,32 @@ public class TaskApplication {
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(System.in);
-			System.out.println("Enter number of tasks");
-			int n = scanner.nextInt();
-			Task[] taskList = new Task[n];
-
-			for (int i = 0; i < n; i++) {
-
-				System.out.println("Enter the Description");
-				String taskDescription = scanner.next();
-				System.out.println("Enter the Due Date in yyyy-MM-dd format");
-				String userInput = scanner.next();
-				DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-				LocalDate date = LocalDate.parse(userInput, dateFormat);
-				Task task = new Task(taskDescription, date);
-
-				taskList[i] = task;
-			}
+			addTask(scanner);
 
 		} catch (Exception e) {
 
 		} finally {
 			scanner.close();
+		}
+	}
+
+
+	private static void addTask(Scanner scanner) {
+		System.out.println("Enter number of tasks");
+		int n = scanner.nextInt();
+		Task[] taskList = new Task[n];
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("Enter the Description");
+			String taskDescription = scanner.next();
+			System.out.println("Enter the Due Date in yyyy-MM-dd format");
+			String userInput = scanner.next();
+			DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate date = LocalDate.parse(userInput, dateFormat);
+			Task task = new Task(taskDescription, date);
+
+			taskList[i] = task;
 		}
 	}
 
